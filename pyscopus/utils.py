f03f648ee8264cf8ef60d421feaf2c9507377afc
Karthik Srinivasan <email.karthik.srinivasan@gmail.com>
2023-03-15 12:26:12 -0400

    Testing

--- pyscopus/utils.py
+++ pyscopus/utils.py
@@ -134,7 +134,7 @@ def _parse_citation(js_citation, year_range):
     return citation_df[columns]
 
 def _parse_affiliation(js_affiliation):
-    l = ""
+    """ l = ""
     for js_affil in js_affiliation:
         if len(l) != 0:
             l = l + "; "
@@ -142,7 +142,14 @@ def _parse_affiliation(js_affiliation):
         city = js_affil['affiliation-city']
         country = js_affil['affiliation-country']
         l = l + name + ", " + city+", "+ country
-    return l
+    return l """
+    l = list()
+    for js_affil in js_affiliation:
+        name = js_affil['affilname']
+        city = js_affil['affiliation-city']
+        country = js_affil['affiliation-country']
+        l.append({'name': name, 'city': city, 'country': country})
+    return 
 
 def _parse_author_affiliation(js_affiliation_entry):
     affiliation_dict = {}
