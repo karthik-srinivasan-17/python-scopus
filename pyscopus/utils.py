e1484241f10144be9e6cc0f8e0ab99139f30a88c
Karthik Srinivasan <email.karthik.srinivasan@gmail.com>
2023-03-24 12:51:21 -0400

    Removed unwanted elements

--- pyscopus/utils.py
+++ pyscopus/utils.py
@@ -820,22 +820,24 @@ def _parse_abstract_retrieval(abstract_entry):
         traceback.print_exc()
         author_keywords = None
     # keys to exclude
-    unwanted_keys = ('dc:creator', 'link','srctype','eid','pubmed-id','prism:coverDate','prism:aggregationType','prism:url',
-                     'source-id','citedby-count','prism:volume','subtype','openaccess','prism:issn','publishercopyright',
+    unwanted_keys = ('dc:identifier','dc:creator','pii','article-number','link','srctype','eid','pubmed-id','prism:coverDate','prism:aggregationType','prism:url',
+                     'source-id','citedby-count','prism:volume','subtype','openaccess','prism:issn',
                       'prism:issueIdentifier','subtypeDescription','prism:pageRange','prism:endingPage','openaccessFlag',
-                       'prism:doi','prism:startingPage','dc:publisher','Scopus-id' )
+                       'prism:doi','prism:startingPage','dc:publisher' )
   
     abstract_dict = {key: coredata[key] for key in coredata.keys()\
                                         if key not in unwanted_keys}
     # rename keys
-    abstract_dict['Scopus-id'] = abstract_dict.pop('dc:identifier').split(':')[-1]
+    #abstract_dict['Scopus-id'] = abstract_dict.pop('dc:identifier').split(':')[-1]
     if "dc:description" in abstract_dict :
         abstract_dict['Abstract'] = abstract_dict.pop('dc:description')
     else:
         abstract_dict['Abstract'] = ""    
     if "publishercopyright" in abstract_dict:
         abstract_dict['Abstract'] = abstract_dict['Abstract'] + ", "+ abstract_dict['publishercopyright']
+        abstract_dict.pop['publishercopyright']
 
+    
     abstract_dict['Abstract Retrieval Title'] = abstract_dict.pop('dc:title')
     abstract_dict['Abbreviated Source Title'] = abbreviated_source_title
     abstract_dict['CODEN'] = coden
